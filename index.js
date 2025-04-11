a309a768b72e206fe4794753f04cce8501a7baea
b122410323 <B122410323@student.utem.edu.my>
2025-04-11 23:25:48 +0800

    Initial commit

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ async function main() {
     // Replace <connection-string> with your MongDB URI
     const uri = "mongodb://localhost:27017/"
     const client = new MongoClient(uri);
+    const start = Date.now(); //Start the timer
 
     try {
         await client.connect();
-        console.log("Connected to MongoDB!");
+        const duration = Date.now() - start; // Calculate duration
+        console.log(`Connected to MongoDB in ${duration} ms`);
 
         const db = client.db("testDB");
         const collection = db.collection("users");
 
 
         // Insert a document
-        await collection.insertOne({ name: "Alice", age: 25 });
+        await collection.insertOne({ name: "Ain Hidayah", age: 23 });
         console.log("Document inserted");
 
         // Query the document
-        const result = await collection.findOne({ name: "Alice"});
+        const result = await collection.findOne({ name: "Ain Hidayah"});
         console.log("Query result:", result);
     } catch (err) {
         console.error("Error:", err);
