bc71a9d161974a1b368924d55b1f685d1e0e11f6
b122410323 <B122410323@student.utem.edu.my>
2025-04-12 12:27:33 +0800

    Initial commit: Setup documentation

--- week 3/week3.js
+++ week 3/week3.js
@@ -1,75 +1,97 @@
-//step 1:Setup
-const express = require('express');
-const { MongoClient } = require('mongodb');
-const port = 3000
+// Install Express Library from the terminal
+// function node is run to code "node index.js"
+
+const express = require ('express'); // Node.js framework to build API's web app
+const { MongoClient, ObjectId } = require('mongodb');
+const port = 3000; // create port and server
 
 const app = express();
 app.use(express.json());
 
 let db;
 
-async function connectToMongoDB() {
-    const uri = "mongodb://localhost:27017";
+async function connectToMongoDB() {             // line 13 sampai 30 connect to mongoDB
+    const uri = "mongodb://localhost:27017/"
     const client = new MongoClient(uri);
 
     try {
         await client.connect();
-        console.log("Connected to MongoDB!");
+        console.log("Connected to MongoDB !");
 
         db = client.db("testDB");
-    }   catch (err) {
+    
+    } catch (err) {
         console.error("Error:", err);
-    }
+    } 
 }
 connectToMongoDB();
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
 
-//STEP 2:Create Ride Endpoints
-//GET /rides - Fetch all rides
-app.get('/rides', async (req, res) => {
+// GET /rides = Fetch all rides
+
+app.get('/rides', async (req, res) => {  // send a GET req to /rides, this fx is triggered
     try {
-        const rides = await db.collection('rides').find().toArray();
-        res.status(200).json(rides);
-    }   catch (err) {
-        res.status(500).json({ error: "Failed to fetch rides"});
-    } 
-});
-
-//PATCH /rides/:id - Update ride status
-app.patch('/rides/:id', async (req, res) => {
-    try{
-        const result = await db.collection('rides').updateOne(
-            { _id: new ObjectId(req.params.id) },
-            {$set: { status:req.body.status } }
-        );
-
-        if (result.modifiedCount ===0) {
-            return res.status(404).json({ error: "Ride not found" });
-        }
-        res.status(200).json({ updated: result.modifiedCount });
-
+        const rides = await db.collection('rides').find().toArray(); // Find all doc in rides collecton & convert the result into an array
+        res.status(200).json(rides); // sends the ride back to client as JSON with status 200 OK
+    
     } catch (err) {
-        //Handle invalid ID format or DB errors
-        res.status(400).json({ error: "Invalid ride ID or data" });
+        res.status(500).json({ error: "Failed to fetch rides" }); // If error happens ( like db issues), it sends a 500 internal server error
     }
 });
 
-//DELETE /rides/:id-Cancel a Ride
-app.delete('/rides/:id', async (req, res) => {
+// POST /rides - Create a new ride
+
+app.post('/rides', async (req, res) => { // Hnadles POST req to create a new ride
     try {
-        const result = await db.collection('rides').deleteOne(
-            {_id: new ObjectId(req.params.id) }
+        const result = await db.collection('rides').insertOne(req.body); // Inserts data from the (req.body) into the ride collection
+        res.status(201).json({ id: result.insertedId }) // send back ID of the newly created ride
+    
+    } catch (err) { 
+        res.status(400).json({ error: "Invalid ride data" }); // If something goes wrong ( missing / bad data ) it returns a 400 Bad req
+    }
+});
+
+// PATCH /rides/:id - Update ride status
+
+app.put('/rides/:id', async (req, res) => { // Handles PATCH req to update a ride by its ID
+    
+    try {
+        const result = await db.collection('rides').updateOne(  // looks for a ride with the matching ID & updates the status field
+            {
+                 _id: new ObjectId(req.params.id) },
+                 { $set: { status: req.body.status }}
+            );
+
+            if (result.modifiedCount === 0) { // if no ride was updated, it returns 404 Not Found
+                return res.status(404).json({ error: "Ride not found"});
+            }
+            res.status(200).json({ updated: result.modifiedCount }); // if successful it returns how many rides were updated (usually 1)
+
+            } catch (err) { // Catches errors like an invalid ID format / bad req data
+
+                // Handle invalid ID format or DB errors
+                res.status(400).json({ error: "Invalid ride ID or data" });
+            }     
+});
+
+// DELETE /rides/:id - Cancel a ride
+
+app.delete('/rides/:id', async (req, res) => { // Handles DELETE req to remove a ride by ID
+    
+    try {
+        const result = await db.collection('rides').deleteOne( // Deletes the ride with the matching ID from the db
+            { _id: new ObjectId(req.params.id) }
         );
 
-        if (result.deletedCount === 0) {
-            return res.status(404).json({error: "Ride not found" });
+        if (result.deletedCount === 0) { // if nothing was deleted, the ride probably didn't exist - return 404
+            return res.status(404).json({ error: "Ride not found"});
         }
-        res.status(200).json({deleted: result.deletedCount });
+        res.status(200).json({ deleted: result.deletedCount}); // on succes, res with hoe many rides were deleted
 
-    } catch (err) {
-        res.status(400).json({ error: "Invalid ride ID"});
+    } catch (err) { // catch & return 400 Bad req for invalid IDs / other errors
+        res.status(400).json({ error: "Invalid ride ID" });
     }
 });
